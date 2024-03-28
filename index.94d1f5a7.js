5f4f8eada709a1ee36aa8a501bbc80358cec4bb6
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-28 13:18:44 +0100

    make build

---
+++ index.94d1f5a7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){var l=e.target;e.preventDefault(),"gallery__img gallery__thumb"===l.className?r.src=l.parentElement.href:"list-item__link"===l.className&&(r.src=l.href)});
+//# sourceMappingURL=index.94d1f5a7.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImage = document.querySelector(".gallery__large-img");
+function changeImage(imageEvent) {
+    const element = imageEvent.target;
+    imageEvent.preventDefault();
+    if (element.className === "gallery__img gallery__thumb") largeImage.src = element.parentElement.href;
+    else if (element.className === "list-item__link") largeImage.src = element.href;
+}
+gallery.addEventListener("click", changeImage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
